45e457c15ddd099a2e046d6712e23ed7e59cf294
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2017-05-11 12:56:15 +0200

    Update AssemblyInfo.cs

--- src/dbup-diy-cli.EntityFramework/Properties/AssemblyInfo.cs
+++ src/dbup-diy-cli.EntityFramework/Properties/AssemblyInfo.cs
@@ -1,36 +1,10 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("dbup-diy-cli.EntityFramework")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyTitle("Helper for running Entity Framework code as DbUp scripts")]
+[assembly: AssemblyCompany("PGS Software")]
 [assembly: AssemblyProduct("dbup-diy-cli.EntityFramework")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: AssemblyCopyright("Copyright © PGS Software 2017")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("af6ab146-1532-420b-845d-71eccae48afe")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
